433677aace001f5d0655990e226957babae2f0e3
doggie52@gmail.com <doggie52@gmail.com@9b2b5166-0481-76c5-5bb7-5e8e65b181e3>
2012-04-09 00:18:39 +0000

    Caching class now makes use of PHP 5's exceptions to throw errors.

---
+++ trunk/class.CacheException.php
@@ -0,0 +1,21 @@
+<?php
+
+	/**
+	 * dG52 PHP Simple Disk Caching class
+	 *
+	 * @author: Douglas Stridsberg
+	 * @email: doggie52@gmail.com
+	 * @url: www.douglasstridsberg.com
+	 *
+	 * Exception class.
+	 */
+
+	/**
+	 * DiskCache
+	 *
+	 * @package diskcache
+	 */
+	class CacheException extends Exception
+	{
+
+	}
